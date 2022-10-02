2308fae3778135fb1113358d799960cae79a0fc6
Lenovo <Lenovo@DESKTOP-U8HCIH4>
2022-10-02 22:10:45 +0300

    Добавьте файлы проекта.

---
+++ WorkWithFile/File.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WorkWithFile
+{
+    class File
+    {
+        public void WorkFile()
+        {   string path = @"C:\Users\Lenovo\Desktop\CSV.txt";
+            string pathEx = @"C:\Users\Lenovo\Desktop\CSV.csv";
+            using (StreamReader reader = new StreamReader(pathEx, Encoding.Default))
+            {
+                string Call;
+                Call = reader.ReadToEnd();
+                Console.WriteLine("Запись завершена");
+                using (FileStream file = new FileStream(path, FileMode.Append))
+            {
+                using (StreamWriter stream = new StreamWriter(file))
+                    
+                    stream.WriteLine(Call);
+            }
+                
+            } 
+
+
+            
+        }
+
+    }
+}
---
+++ WorkWithFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WorkWithFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File file = new File();
+            file.WorkFile();
+        }
+    }
+}
